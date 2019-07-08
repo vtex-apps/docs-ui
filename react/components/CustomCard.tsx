a000627615b4799679ceda2c891f50ddbb0a7ec4
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-08 13:31:51 -0300

    Make the padding smaller on `CustomCard`

--- react/components/CustomCard.tsx
+++ react/components/CustomCard.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react'
 const CustomCard: FunctionComponent = ({ children }) => (
   <div
     style={{ boxShadow: '0 3px 9px 0 rgba(61, 62, 64, 0.2)' }}
-    className='vtex-card card w-100 b2 br2 bg-base--inverted c-on-base pa6'>
+    className='vtex-card card w-100 b2 br2 bg-base--inverted c-on-base pa5'>
     {children}
   </div>
 )
