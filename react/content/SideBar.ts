bfc7f5c856d87e9137970941eb3937be8d11371a
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-08 13:30:39 -0300

    Create `/react/content` for the static content

---
+++ react/content/SideBar.ts
@@ -0,0 +1,9 @@
+export const items = [
+  { text: 'Recipes', link: '/docs/recipes' },
+  { text: 'Components', link: '/docs/components' },
+  { text: 'Releases', link: '/docs/releases' },
+  { text: 'Apps', link: '/docs/apps' },
+  { text: 'Getting Started', link: '/docs/getting-started' },
+  { text: 'Community', link: 'https://github.com/vtex-apps/store-discussion' },
+  { text: 'Toolbelt', link: '/docs/toolbelt' },
+]
