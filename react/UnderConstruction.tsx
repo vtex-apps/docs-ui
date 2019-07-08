a9deb4e6ee544263c0679fd23cebb4644b9a37d3
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-08 12:21:34 -0300

    Add `UnderConstruction` page

---
+++ react/UnderConstruction.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment, FunctionComponent } from 'react'
+import { Helmet } from 'vtex.render-runtime'
+import { EmptyState } from 'vtex.styleguide'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import favicon from './images/favicon.png'
+
+const Docs: FunctionComponent<any> = () => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Docs</title>
+        <meta name="theme-color" content="#F71963" />
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <Navbar />
+      <main className="w-100 pv10 min-vh-75 bg-base--inverted">
+        <EmptyState title="Under Construction">
+          <p>This page is still being developed.</p>
+          <p>Come back in a bit and something awesome should be here!</p>
+        </EmptyState>
+      </main>
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default Docs
