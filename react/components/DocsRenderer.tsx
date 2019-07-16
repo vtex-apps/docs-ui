fd86beb13dbc24fbe7c8a0880f470c4cfa813421
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-16 12:54:55 -0300

    Update Query used by DocsRenderer to use version from AppVersionState

--- react/components/DocsRenderer.tsx
+++ react/components/DocsRenderer.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import remark from 'remark'
 import remark2react from 'remark-react'
-import { graphql, compose } from 'react-apollo'
-import { branch, renderComponent } from 'recompose'
-import { withRuntimeContext } from 'vtex.render-runtime'
+import { Query } from 'react-apollo'
+import { ApolloError } from 'apollo-client'
+import { useRuntime } from 'vtex.render-runtime'
 
 import Skeleton from './Skeleton'
 import EmptyDocs from './EmptyDocs'
 import { remarkReactComponents } from './CustomTags'
+import { useAppVersionState } from './AppVersionContext'
 
 import * as MarkdownFile from '../graphql/getMarkdownFile.graphql'
 
@@ -19,54 +20,57 @@ interface MetaData {
   git: string
 }
 
-const DocsRenderer: FunctionComponent<any> = ({ markdownQuery }) => {
+const DocsRenderer: FunctionComponent = () => {
+  const { major } = useAppVersionState()
   const {
-    meta,
-    markdown,
-  }: { meta: MetaData; markdown: string } = markdownQuery.getMarkdownFile
+    route: { params },
+  } = useRuntime()
+  const { app, file } = params
+  const [appName] = app.split('@')
+  const fileName = file || 'README.md'
+
+  const finalAppName = `${appName}@${major}.x`
 
   return (
-    <article className="ph9 w-100">
-      {meta.tags && <p>Tags: {meta.tags.toString()}</p>}
-      {
-        remark()
-          .use(remark2react, {
-            remarkReactComponents,
-          })
-          .processSync(markdown).contents
-      }
-      {meta.git && (
-        <p>
-          Edit this page on <a href={meta.git}>GitHub</a>
-        </p>
-      )}
-    </article>
+    <Query
+      query={MarkdownFile.default}
+      variables={{ appName: finalAppName, fileName }}>
+      {({
+        loading,
+        error,
+        data,
+      }: {
+        loading: boolean
+        error?: ApolloError
+        data: { getMarkdownFile: { markdown: string; meta: MetaData } }
+      }) => {
+        if (loading) return <Skeleton />
+        if (error) return <EmptyDocs />
+
+        const { markdown, meta } = data.getMarkdownFile
+
+        if (markdown === '') return <EmptyDocs />
+
+        return (
+          <article className="ph9 w-100 min-vh-100">
+            {meta.tags && <p>Tags: {meta.tags.toString()}</p>}
+            {
+              remark()
+                .use(remark2react, {
+                  remarkReactComponents,
+                })
+                .processSync(markdown).contents
+            }
+            {meta.git && (
+              <p>
+                Edit this page on <a href={meta.git}>GitHub</a>
+              </p>
+            )}
+          </article>
+        )
+      }}
+    </Query>
   )
 }
 
-export default compose(
-  withRuntimeContext,
-  graphql(MarkdownFile.default, {
-    name: 'markdownQuery',
-    options: (props: { runtime: any }) => {
-      const { app, file } = props.runtime.route.params
-      const fileName = file || 'README.md'
-      const version = props.runtime.query.version
-      const appName = `${app}${version ? `@${version}` : ''}`
-      return {
-        variables: {
-          appName,
-          fileName,
-        },
-      }
-    },
-  }),
-  branch(
-    ({ markdownQuery }: any) => markdownQuery.loading,
-    renderComponent(Skeleton)
-  ),
-  branch(
-    ({ markdownQuery }: any) => !!markdownQuery.error,
-    renderComponent(EmptyDocs)
-  )
-)(DocsRenderer)
+export default DocsRenderer
