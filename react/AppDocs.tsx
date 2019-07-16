ac6342d803eed1969226580dcccfa825dbf9d756
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-16 12:53:51 -0300

    Add version selector to AppDocs page and use AppVersionProvider

--- react/AppDocs.tsx
+++ react/AppDocs.tsx
@@ -5,9 +5,11 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
 import DocsRenderer from './components/DocsRenderer'
+import { EnhancedAppVersionProvider } from './components/AppVersionContext'
+import VersionSelector from './components/VersionSelector'
 import favicon from './images/favicon.png'
 
-const Docs: FunctionComponent = () => {
+const AppDocs: FunctionComponent = () => {
   return (
     <Fragment>
       <Helmet>
@@ -17,16 +19,21 @@ const Docs: FunctionComponent = () => {
       </Helmet>
       <Navbar />
       <main className="w-100 bg-base--inverted flex">
-        <div className="bg-muted-1 w-20">
-          <SideBar />
-        </div>
-        <div className="pv10 w-80-l w-90 center">
-          <DocsRenderer />
-        </div>
+        <EnhancedAppVersionProvider>
+          <div className="bg-muted-1 w-20">
+            <SideBar />
+          </div>
+          <div className="pv10 w-80-l w-90 center flex flex-column">
+            <div className="self-end ph9">
+              <VersionSelector />
+            </div>
+            <DocsRenderer />
+          </div>
+        </EnhancedAppVersionProvider>
       </main>
       <Footer />
     </Fragment>
   )
 }
 
-export default Docs
+export default AppDocs
