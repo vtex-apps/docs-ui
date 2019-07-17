3e3158650f90b9a254dc897327afeea143c05aa7
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 15:00:15 -0300

    Add RightArrow icon

---
+++ react/components/icons/RightArrow.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightArrow = () => (
+  <svg
+    width="59"
+    height="14"
+    viewBox="0 0 59 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 7H57" stroke="#000" />
+    <path d="M49 1L57.5 7L49 13" stroke="#000" />
+  </svg>
+)
+
+export default RightArrow
