42cb427498fcb2e746989fa8060ae50730149472
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 15:01:36 -0300

    Create Community and LatestFeatures components for the homepage

---
+++ react/components/Community.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Community = () => (
+  <section className="mv9">
+    <h2 className="t-heading-2 normal">Need help?</h2>
+    <div className="flex items-center">
+      <div className="bg-muted-2" style={{ width: '242px', height: '120px' }} />
+      <div className="ml4">
+        <p className="t-heading-4">Join our community!</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod.
+        </p>
+      </div>
+    </div>
+  </section>
+)
+
+export default Community
