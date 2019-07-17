159856066321cd8650147413257f87f984285973
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 19:53:52 -0300

    Add ExternalLink icon

---
+++ react/components/icons/ExternalLink.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ExternalLink = () => (
+  <svg
+    width="10"
+    height="10"
+    viewBox="0 0 10 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9.375 0H5V1.25H7.875L3.9375 5.1875L4.8125 6.0625L8.75 2.125V5H10V0.625C10 0.25 9.75 0 9.375 0Z"
+      fill="#797979"
+    />
+    <path
+      d="M8.75 10H0.625C0.25 10 0 9.75 0 9.375V1.25C0 0.875 0.25 0.625 0.625 0.625H3.125V1.875H1.25V8.75H8.125V6.875H9.375V9.375C9.375 9.75 9.125 10 8.75 10Z"
+      fill="#797979"
+    />
+  </svg>
+)
+
+export default ExternalLink
