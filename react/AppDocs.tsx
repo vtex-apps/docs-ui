860e66029116e9af4bb6d7d31d383f2b3b57a373
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 14:56:48 -0300

    Delete unnecessary components

--- react/AppDocs.tsx
+++ react/AppDocs.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, FunctionComponent } from 'react'
 import { Helmet } from 'vtex.render-runtime'
 
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
 import DocsRenderer from './components/DocsRenderer'
@@ -17,7 +16,6 @@ const AppDocs: FunctionComponent = () => {
         <meta name="theme-color" content="#F71963" />
         <link rel="icon" href={favicon} />
       </Helmet>
-      <Navbar />
       <main className="w-100 bg-base flex">
         <EnhancedAppVersionProvider>
           <div className="bg-muted-1 w-20">
