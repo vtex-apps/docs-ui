2167c6b73da31d0175770d8f3634cfcc833e0fda
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 14:59:58 -0300

    Add placeholder content for the Docs homepage

---
+++ react/content/Latest.ts
@@ -0,0 +1,17 @@
+export const latest = [
+  {
+    title: 'Storefront page customization by URL',
+    description:
+      'Customize your store through Storefront faster by simply navigating to the desired page using its URL.',
+  },
+  {
+    title: 'Product Identifier Component',
+    description:
+      'Display products/SKU IDs and reference anywhere on your Product page, Shelf and Search Results.',
+  },
+  {
+    title: 'Storefront page customization by URL',
+    description:
+      'Use its new prop as true and enable store users to search by the desired category, brand and collection.',
+  },
+]
