7da92eba7ec30d83d2224554a097f8c3e053e09b
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 20:05:41 -0300

    Update Footer layout`

--- react/components/Footer.tsx
+++ react/components/Footer.tsx
@@ -1,77 +1,54 @@
 import React, { FunctionComponent } from 'react'
 import { FormattedMessage } from 'react-intl'
 
-import Logo from '../images/VTEX_footer.svg'
-import VTEXOffices from '../VTEXOffices.json'
+import VTEXBlack from './icons/VTEXBlack'
+import ExternalLink from './icons/ExternalLink'
 
-const footerLinks = [
-  {
-    id: 'pricing',
-    options: [],
-  },
-  {
-    id: 'company',
-    options: ['carrers', 'partnersAWS', 'dmca', 'blog'],
-  },
-  {
-    id: 'resources',
-    options: [
-      'devdocs',
-      'support',
-      'announcements',
-      'releases',
-      'status',
-      'health',
-      'vtexio',
-    ],
-  },
-  {
-    id: 'partners',
-    options: ['vtex', 'find', 'program', 'appstore'],
-  },
-]
+const Footer: FunctionComponent = () => {
+  const listItemClasses = 't-body mv3 c-on-muted-3'
 
-const Footer: FunctionComponent = () => (
-  <footer className="bg-base pa10-l pv7 c-muted-1">
-    <div className="flex flex-wrap justify-between">
-      <div className="w-33-l ph4 ph0-l">
-        <img src={Logo} alt="VTEX" />
-        <p className="flex">
-          <FormattedMessage id="io.footer.vtex" />
+  return (
+    <footer className="bg-base pa7-l w-100 pv7 bg-muted-3">
+      <div className="pa6">
+        <VTEXBlack />
+      </div>
+      <div className="flex-l ph4 flex-wrap dn-s justify-between w-100">
+        <div className="flex">
+          <ul className="list">
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/getting-started" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/recipes" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/components" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/resources" />
+            </li>
+          </ul>
+          <ul className="list">
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/community" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/latest-features" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/apis" />
+            </li>
+            <li className={listItemClasses}>
+              <FormattedMessage id="docs/partner" />
+            </li>
+          </ul>
+        </div>
+        <p className="self-baseline c-on-muted-3">
+          <FormattedMessage id="docs/visit-us" /> <ExternalLink />
         </p>
       </div>
-      <div className="flex-l flex-wrap dn-s">
-        {footerLinks.map(category => {
-          const baseMessageId = `io.footer.${category.id}`
-          return (
-            <ul key={category.id}>
-              <p className="fw7">
-                <FormattedMessage id={baseMessageId} />
-              </p>
-              {category.options.map(option => (
-                <li className="list" key={option}>
-                  <p>
-                    <FormattedMessage id={`${baseMessageId}.${option}`} />
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )
-        })}
-      </div>
-    </div>
-    <div className="flex flex-wrap center mv7">
-      {VTEXOffices.Offices.map(office => (
-        <div key={office.short} className="ph4 w-20-ns w-50-s">
-          <p className="t-heading-3">{office.short}</p>
-          <p>{office.address1}</p>
-          <p>{office.address2}</p>
-          {office.address3 && <p>{office.address3}</p>}
-          <p>{office.city}</p>
-        </div>
-      ))}
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
