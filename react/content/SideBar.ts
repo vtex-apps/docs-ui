2d1f824f885b4aeb4fede79dcc30850df86c2d38
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-17 14:59:58 -0300

    Add placeholder content for the Docs homepage

--- react/content/SideBar.ts
+++ react/content/SideBar.ts
@@ -1,9 +1,47 @@
 export const items = [
-  { text: 'Recipes', link: '/docs/recipes' },
-  { text: 'Components', link: '/docs/components' },
-  { text: 'Releases', link: '/docs/releases' },
-  { text: 'Apps', link: '/docs/apps' },
-  { text: 'Getting Started', link: '/docs/getting-started' },
-  { text: 'Community', link: 'https://github.com/vtex-apps/store-discussion' },
-  { text: 'Toolbelt', link: '/docs/toolbelt' },
+  {
+    text: 'Getting Started',
+    link: '/docs/getting-started',
+    articles: [],
+  },
+  {
+    text: 'Recipes',
+    link: '/docs/recipes',
+    articles: [
+      { text: 'Style', link: '', articles: [] },
+      { text: 'Layout', link: '', articles: [] },
+      { text: 'Content', link: '', articles: [] },
+      { text: 'Routes', link: '', articles: [] },
+      { text: 'Plugins', link: '', articles: [] },
+      { text: 'Custom Blocks', link: '', articles: [] },
+      { text: 'All Recipes', link: '', articles: [] },
+    ],
+  },
+  {
+    text: 'Components',
+    link: '/docs/components',
+    articles: [
+      { text: 'General', link: '', articles: [] },
+      { text: 'Navigation', link: '', articles: [] },
+      { text: 'Product Related', link: '', articles: [] },
+      { text: 'Search Related', link: '', articles: [] },
+      { text: 'All Components', link: '', articles: [] },
+    ],
+  },
+  {
+    text: 'Resources',
+    link: '/docs/resources',
+    articles: [
+      { text: 'General', link: '', articles: [] },
+      { text: 'Navigation', link: '', articles: [] },
+      { text: 'Product Related', link: '', articles: [] },
+      { text: 'Search Related', link: '', articles: [] },
+      { text: 'All Components', link: '', articles: [] },
+    ],
+  },
+  {
+    text: 'Lastest features',
+    link: '/docs/latest',
+    articles: [],
+  },
 ]
