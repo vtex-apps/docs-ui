ff6b78b09b0f57eed3e76ef249261c2a0b3bb6e0
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-18 20:46:27 -0300

    Update `useAppName` hook so that it holds all necessary logic to Query documentation

--- react/hooks/useAppName.tsx
+++ react/hooks/useAppName.tsx
@@ -1,15 +1,19 @@
 import { useRuntime } from 'vtex.render-runtime'
+import { useAppVersionState } from '../components/AppVersionContext'
 
-export function useAppNameAndFile(major: string) {
+export function useAppNameAndFile() {
   const {
     route: { params },
   } = useRuntime()
 
   const { app, file } = params
-  const [appName] = app.split('@')
+  const appName = app ? app.split('@')[0] : null
   const fileName = file || 'README.md'
 
-  const finalAppName = `${appName}@${major}.x`
+  /* eslint-disable react-hooks/rules-of-hooks */
+  const major = appName ? useAppVersionState().major : null
+
+  const finalAppName = major ? `${appName}@${major}.x` : undefined
 
   return { appName: finalAppName, fileName }
 }
