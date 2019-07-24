c044fc0c7207c79913b7aa7077c43323f13c37f8
victorhmp <victor2142@gmail.com>
2019-07-24 11:36:16 -0300

    Simplify DocsRenderer component

--- react/components/DocsRenderer.tsx
+++ react/components/DocsRenderer.tsx
@@ -1,15 +1,25 @@
 import React, { FunctionComponent } from 'react'
 import ReactMarkdown from 'react-markdown'
-import { Query } from 'react-apollo'
-import { ApolloError } from 'apollo-client'
 import { FormattedMessage } from 'react-intl'
 
-import Skeleton from './Skeleton'
-import EmptyDocs from './EmptyDocs'
 import { CustomRenderers } from './CustomTags'
-import { useAppNameAndFile } from '../hooks/useAppName'
 
-import * as MarkdownFile from '../graphql/getMarkdownFile.graphql'
+const DocsRenderer: FunctionComponent<Props> = ({ markdown, meta }) => {
+  return (
+    <article className="ph9 w-100 min-vh-100">
+      <ReactMarkdown
+        source={markdown}
+        escapeHtml={false}
+        renderers={CustomRenderers}
+      />
+      {meta.git && (
+        <a href={meta.git}>
+          <FormattedMessage id="docs/renderer-github" />
+        </a>
+      )}
+    </article>
+  )
+}
 
 interface MetaData {
   title: string
@@ -19,44 +29,9 @@ interface MetaData {
   git: string
 }
 
-const DocsRenderer: FunctionComponent = () => {
-  const { appName, fileName } = useAppNameAndFile()
-
-  return (
-    <Query query={MarkdownFile.default} variables={{ appName, fileName }}>
-      {({
-        loading,
-        error,
-        data,
-      }: {
-        loading: boolean
-        error?: ApolloError
-        data: { getMarkdownFile: { markdown: string; meta: MetaData } }
-      }) => {
-        if (loading) return <Skeleton />
-        if (error) return <EmptyDocs />
-
-        const { markdown, meta } = data.getMarkdownFile
-
-        if (markdown === '') return <EmptyDocs />
-
-        return (
-          <article className="ph9 w-100 min-vh-100">
-            <ReactMarkdown
-              source={markdown}
-              escapeHtml={false}
-              renderers={CustomRenderers}
-            />
-            {meta.git && (
-              <a href={meta.git}>
-                <FormattedMessage id="docs/renderer-github" />
-              </a>
-            )}
-          </article>
-        )
-      }}
-    </Query>
-  )
+interface Props {
+  markdown: string
+  meta: MetaData
 }
 
 export default DocsRenderer
