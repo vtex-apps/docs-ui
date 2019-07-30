659289b9a0acc7299ec8a910dbfca7e6e227b709
victorhmp <victor2142@gmail.com>
2019-07-30 15:50:27 -0300

    Add link to Store Discussion in the Community component

--- react/components/Community.tsx
+++ react/components/Community.tsx
@@ -11,19 +11,23 @@ const Community: FunctionComponent<InjectedIntlProps> = ({ intl }) => (
       className="t-heading-2 normal">
       <FormattedMessage id="docs/community-help" />
     </h2>
-    <div className="flex items-center">
-      <div className="w-10-l w-25 mr6">
-        <CommunityIcon />
+    <a
+      href="https://github.com/vtex-apps/store-discussion"
+      className="link no-underline">
+      <div className="flex items-center">
+        <div className="w-10-l w-25 mr6">
+          <CommunityIcon />
+        </div>
+        <div className="ml6">
+          <h4 className="t-heading-4 c-on-base">
+            <FormattedMessage id="docs/community-join" />
+          </h4>
+          <p className="t-body c-on-base">
+            <FormattedMessage id="docs/lorem-short" />
+          </p>
+        </div>
       </div>
-      <div className="ml6">
-        <p className="t-heading-4">
-          <FormattedMessage id="docs/community-join" />
-        </p>
-        <p>
-          <FormattedMessage id="docs/lorem-short" />
-        </p>
-      </div>
-    </div>
+    </a>
   </section>
 )
 
