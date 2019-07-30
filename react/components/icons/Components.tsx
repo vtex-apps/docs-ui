2d3da836790ba1b8ff65a8407a65820d69aef3a0
victorhmp <victor2142@gmail.com>
2019-07-30 14:03:22 -0300

    Create new icons

---
+++ react/components/icons/Components.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Components() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M20 2H5v25h15V2z"
+      />
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M20 33H5v13h15V33zM43 2H26v13h17V2z"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M43 21H26v25h17V21z"
+      />
+    </svg>
+  )
+}
+
+export default Components
---
+++ react/components/icons/Recipes.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Recipes() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M14 12h2M22 12h12M14 20h2M22 20h12M14 28h2M22 28h12M14 36h10"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M30 46V34h12"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M30 46H6V2h36v32L30 46z"
+      />
+    </svg>
+  )
+}
+
+export default Recipes
