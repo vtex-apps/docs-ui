f6e61b2b25cfc91c634b46ffaa13ffaf407bd43a
victorhmp <victor2142@gmail.com>
2019-07-30 15:29:04 -0300

    SideBar item now signals when it is active

--- react/components/SideBarItem.tsx
+++ react/components/SideBarItem.tsx
@@ -1,7 +1,8 @@
 import React, { useState, FunctionComponent } from 'react'
 
-import { Link } from 'vtex.render-runtime'
+import { Link, useRuntime } from 'vtex.render-runtime'
 import { IconCaretDown, IconCaretRight } from 'vtex.styleguide'
+import { slug } from '../utils'
 
 interface Props {
   text: string
@@ -19,34 +20,45 @@ const SideBarItem: FunctionComponent<Props> = ({
   hasArticles,
   children,
 }) => {
-  const [open, setOpen] = useState(false)
+  const activeUrl = useRuntime().route.path
   const isIODocs = appName && appName.split('@')[0] === 'vtex.io-documentation'
   const linkUrl =
     link && `/docs${!isIODocs && appName ? `/${appName}` : ''}/${link}`
+  const isActive = linkUrl === activeUrl
+  const activeCategory = activeUrl.split('/')[2]
+  const activeSubCategory = activeUrl.split('/')[3]
+  const hasActiveChildren =
+    slug(text.toLowerCase()) === activeCategory ||
+    slug(text.toLowerCase()) === activeSubCategory
+  const [open, setOpen] = useState(hasActiveChildren || isActive)
 
   const ZeroDepthItem = () =>
-    text !== 'Introduction' ? <h5 className="mv4 t-heading-5">{text}</h5> : null
+    text !== 'Introduction' ? <div className="mv4 t-body">{text}</div> : null
 
-  const NormalItem = () => <div className="c-muted-2 mv3">{text}</div>
+  const NormalItem = () => <div className="mv3 t-body">{text}</div>
 
   return (
     <div className="link">
       <div
         className={`flex justify-between items-center pointer ${
-          depth >= 2 ? 'b--muted-3 bl bw1 pl4 t-small' : ''
+          depth >= 2 ? 'pl4 t-small' : ''
         }`}
         onClick={() => setOpen(!open)}
         onKeyPress={() => setOpen(!open)}
         role="menuitem"
         tabIndex={-1}>
         {linkUrl ? (
-          <Link to={linkUrl} className="no-underline c-on-base">
+          <Link
+            to={linkUrl}
+            className={`no-underline ${isActive ? 'c-emphasis' : 'c-on-base'}`}>
             {depth === 0 ? <ZeroDepthItem /> : <NormalItem />}
           </Link>
         ) : depth === 0 ? (
           <ZeroDepthItem />
         ) : (
-          <NormalItem />
+          <div className={`${isActive ? 'c-emphasis' : 'c-on-base'}`}>
+            <NormalItem />
+          </div>
         )}
         <div className="ph2">
           {hasArticles &&
