dcdca513b93fb661aa3ede23f2357506573b38cb
victorhmp <victor2142@gmail.com>
2019-07-30 15:24:15 -0300

    Update UnderConstruction to fill the entire page height

--- react/UnderConstruction.tsx
+++ react/UnderConstruction.tsx
@@ -6,10 +6,12 @@ import PageLayoutContainer from './components/PageLayoutContainer'
 const UnderConstruction: FunctionComponent = () => {
   return (
     <PageLayoutContainer>
-      <EmptyState title="Under Construction">
-        <p>This page is still being developed.</p>
-        <p>Come back in a bit and something awesome should be here!</p>
-      </EmptyState>
+      <div className="min-vh-100 w-100 flex justify-center items-center">
+        <EmptyState title="Under Construction">
+          <p>This page is still being developed.</p>
+          <p>Come back in a bit and something awesome should be here!</p>
+        </EmptyState>
+      </div>
     </PageLayoutContainer>
   )
 }
