1b878357546ce098eecf04652d97261663d6bdc4
victorhmp <victor2142@gmail.com>
2019-07-30 14:03:22 -0300

    Create new icons

---
+++ react/components/icons/Resources.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function Resources() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48">
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M2 32h12M14 32h12M31.509 32.107l11.893-1.598"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M14 4H2v40h12V4z"
+      />
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M8 10v10"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M26 4H14v40h12V4z"
+      />
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M20 10v10"
+      />
+      <path
+        stroke="#142032"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M39.675 2.76L27.782 4.357 33.106 44l11.893-1.597L39.675 2.76z"
+      />
+      <path
+        stroke="#F71963"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M34.527 9.504l1.331 9.911"
+      />
+    </svg>
+  )
+}
+
+export default Resources
