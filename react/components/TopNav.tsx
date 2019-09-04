a19d7d514c738db7aaf5e54df3abed5bf8a80e8b
victorhmp <victor2142@gmail.com>
2019-09-04 19:02:30 -0300

    Update TopNav color and VTEX logo

--- react/components/TopNav.tsx
+++ react/components/TopNav.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
 const TopNav = () => (
-  <nav className="w-100 db-l dn bg-base--inverted">
+  <nav className="w-100 db-l dn bg-base bb b--muted-4">
     <div className="flex w-100 justify-end">
       <div className="flex c-on-base--inverted pv5">
         <a
           href="https://help.vtex.com/developer-docs"
-          className="link c-on-base--inverted no-underline mv3 mh5">
+          className="t-small link c-on-base no-underline mv3 mh5">
           <FormattedMessage id="docs/nav.api" />
         </a>
         <a
           href="https://github.com/vtex-apps/release-notes"
-          className="link c-on-base--inverted no-underline mv3 mh5">
+          className="t-small link c-on-base no-underline mv3 mh5">
           <FormattedMessage id="docs/nav.latest" />
         </a>
         <a
           href="https://www.vtex.com/partner/"
-          className="link c-on-base--inverted no-underline mv3 mh5">
+          className="t-small link c-on-base no-underline mv3 mh5">
           <FormattedMessage id="docs/nav.partner" />
         </a>
       </div>
