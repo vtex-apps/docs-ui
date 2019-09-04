4f73f833052aeca2e6f231e8e5904e27ee10232b
victorhmp <victor2142@gmail.com>
2019-09-04 20:51:13 -0300

    Fix LeftArrow component

--- react/components/icons/LeftArrow.tsx
+++ react/components/icons/LeftArrow.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const RightArrow = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 59 14">
+const LeftArrow = () => (
+  <svg
+    width="59"
+    height="14"
+    viewBox="0 0 59 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
     <path stroke="#F71963" d="M58.5 7h-57M9.5 13L1 7l8.5-6" />
   </svg>
 )
 
-export default RightArrow
+export default LeftArrow
