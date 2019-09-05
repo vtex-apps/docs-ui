57a9c1beb64e4c6e55b5b15fc8350b9b5f0095bc
victorhmp <victor2142@gmail.com>
2019-09-05 19:25:20 -0300

    Add new EmptyDocs title

--- react/components/EmptyDocs.tsx
+++ react/components/EmptyDocs.tsx
@@ -1,11 +1,23 @@
 import React, { FC } from 'react'
-import { FormattedMessage } from 'react-intl'
+import {
+  FormattedMessage,
+  injectIntl,
+  defineMessages,
+  InjectedIntlProps,
+} from 'react-intl'
 
 import { EmptyState } from 'vtex.styleguide'
 
-const EmptyDocs: FC = () => (
+defineMessages({
+  emptyStateTitle: {
+    id: 'docs/empty.title',
+    defaultMessage: '',
+  },
+})
+
+const EmptyDocs: FC<InjectedIntlProps> = ({ intl }) => (
   <div className="pa7 w-100 flex justify-center">
-    <EmptyState title="Something went wrong">
+    <EmptyState title={intl.formatMessage({ id: 'docs/empty.title' })}>
       <p>
         <FormattedMessage id="docs/empty-docs" />
       </p>
@@ -13,4 +25,4 @@ const EmptyDocs: FC = () => (
   </div>
 )
 
-export default EmptyDocs
+export default injectIntl(EmptyDocs)
