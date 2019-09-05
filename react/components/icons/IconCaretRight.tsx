7f2dbbd83329122878cb914e51ade12e8fe3b457
victorhmp <victor2142@gmail.com>
2019-09-05 13:49:17 -0300

    Update Caret icons

---
+++ react/components/icons/IconCaretRight.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const IconCaretRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16">
+    <path
+      fill="#3f3f40"
+      className="0"
+      d="M5.108 16.41L3.59 15.108 9.683 8 3.59.892 5.108-.41l6.651 7.76a1 1 0 010 1.301L5.108 16.41z"
+    />
+  </svg>
+)
+
+export default IconCaretRight
