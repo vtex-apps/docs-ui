683ab3c53fa93018f055fc87f780701980626dce
João Marcos Gris <joao.gris@vtex.com.br>
2019-11-14 12:29:01 -0300

    Use docs title as page title

--- react/components/DocsRenderer.tsx
+++ react/components/DocsRenderer.tsx
@@ -1,28 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, Fragment } from 'react'
 import ReactMarkdown from 'react-markdown'
 import emoji from 'remark-emoji'
 import { FormattedMessage } from 'react-intl'
 
 import { CustomRenderers } from './CustomTags'
 import EmptyDocs from './EmptyDocs'
+import { Helmet } from 'vtex.render-runtime'
 
 const DocsRenderer: FC<Props> = ({ markdown, meta }) => {
   const isEmptyDocs = markdown === ''
-
+  const title = markdown
+    .split('\n')[0]
+    .replace('#', '')
+    .trim()
   return !isEmptyDocs ? (
-    <article className="ph0-l ph5 w-100-l w-90 min-vh-100">
-      <ReactMarkdown
-        source={markdown}
-        escapeHtml={false}
-        renderers={CustomRenderers}
-        plugins={[emoji]}
-      />
-      {meta.git && (
-        <a href={meta.git} className="c-emphasis no-underline dim flex mt8">
-          <FormattedMessage id="docs/renderer-github" />
-        </a>
-      )}
-    </article>
+    <Fragment>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <article className="ph0-l ph5 w-100-l w-90 min-vh-100">
+        <ReactMarkdown
+          source={markdown}
+          escapeHtml
+          renderers={CustomRenderers}
+          plugins={[emoji]}
+        />
+        {meta.git && (
+          <a href={meta.git} className="c-emphasis no-underline dim flex mt8">
+            <FormattedMessage id="docs/renderer-github" />
+          </a>
+        )}
+      </article>
+    </Fragment>
   ) : (
     <EmptyDocs />
   )
