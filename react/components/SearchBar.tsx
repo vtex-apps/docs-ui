9b9c2f1adf32453708346f3a0049502e73cbab92
João Marcos Gris <joao.gris@vtex.com.br>
2019-12-01 00:02:03 -0300

    Search bar logic

---
+++ react/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useState, Fragment } from 'react'
+import { InputSearch } from 'vtex.styleguide'
+import { useRuntime } from 'vtex.render-runtime'
+const SearchBar: FC = () => {
+  const {
+    navigate,
+    query: {
+      q: queryString
+    },
+  } = useRuntime()
+  const [inputString, setInputString] = useState(queryString || '')
+  return (
+    <Fragment>
+      <InputSearch
+        placeholder="How to use a shelf"
+        value={inputString}
+        size="large"
+        onChange={(e: { target: { value: React.SetStateAction<string> } }) => {
+          setInputString(e.target.value)
+        }}
+        onSubmit={(e: any) => {
+          e.preventDefault()
+          navigate({ page: 'docs.search', query: `q=${inputString}`, fetchPage: true })
+        }}
+      />
+    </Fragment>
+  )
+}
+export default SearchBar
--- react/components/TopNav.tsx
+++ react/components/TopNav.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Link } from 'vtex.render-runtime'
+import { Link, useRuntime } from 'vtex.render-runtime'
+import SearchBar from './SearchBar'
+import { Button, IconSearch } from 'vtex.styleguide'
 
-const TopNav = () => (
-  <nav className="w-100 db-l dn bg-base bb b--muted-3">
-    <div className="flex w-100 justify-end">
-      <div className="flex c-on-base--inverted pv5">
-        <a
-          href="https://help.vtex.com/developer-docs"
-          className="t-small link c-on-base no-underline mv3 mh5 dim">
-          <FormattedMessage id="docs/nav.api" />
-        </a>
-        <Link
-          to="/docs#latest-releases"
-          className="t-small link c-on-base no-underline mv3 mh5 dim">
-          <FormattedMessage id="docs/latest-features" />
-        </Link>
-        <a
-          href="https://www.vtex.com/partner/"
-          className="t-small link c-emphasis no-underline mv3 mh5 dim">
-          <FormattedMessage id="docs/nav.partner" />
-        </a>
-      </div>
-    </div>
-  </nav>
-)
-
+const TopNav = () => {
+  const {
+    route: { id: routeId, path: pathString },
+  } = useRuntime()
+  const [onSearch, setOnSearch] = useState(() => routeId === 'docs.search')
+  return (
+    <nav className="w-100 db-l dn bg-base bb b--muted-3">
+      {onSearch ? <SearchBar /> :
+        <div className="flex w-100 justify-end">
+          <div className="flex c-on-base--inverted pv5">
+            <a
+              href="https://help.vtex.com/developer-docs"
+              className="t-small link c-on-base no-underline mv3 mh5 dim">
+              <FormattedMessage id="docs/nav.api" />
+            </a>
+            <Link
+              to="/docs#latest-releases"
+              className="t-small link c-on-base no-underline mv3 mh5 dim">
+              <FormattedMessage id="docs/latest-features" />
+            </Link>
+            <a
+              href="https://www.vtex.com/partner/"
+              className="t-small link c-emphasis no-underline mv3 mh5 dim">
+              <FormattedMessage id="docs/nav.partner" />
+            </a>
+            <Link to={pathString} onClick={() => setOnSearch(true)} className="mh5 mv3 no-underline link c-emphasis" ><IconSearch /></Link>
+          </div>
+        </div>
+      }
+    </nav>
+  )
+}
 export default TopNav
