fe90891efc008ca8ff6f6618ee316ddfdfa6e278
João Marcos Gris <joao.gris@vtex.com.br>
2019-11-30 23:46:20 -0300

    Search Results page

---
+++ react/Search.tsx
@@ -0,0 +1,51 @@
+import React, { FC, useState, useEffect, Fragment } from 'react'
+import { Link, Card } from 'vtex.styleguide'
+import { cseSearch } from './clients/cse'
+
+
+interface Props {
+  query: {
+    q: string
+  }
+}
+
+interface SearchResult {
+  title: string,
+  snippet: string,
+  link: string
+}
+
+const Search: FC<Props> = ({ query }) => {
+  const [results, setResults] = useState<SearchResult[]>([])
+  const queryString = query.q || ''
+  useEffect(() => {
+    (
+      async () => {
+        setResults(await cseSearch(queryString))
+      })()
+  }, [query])
+  return (
+    <Fragment>
+      <h1 className="center">Results for <span className="c-emphasis">{queryString}</span></h1>
+      <ul className="list pl0 center mt8 mw8">
+        {results ? results.map((result: any, index: number) => (
+          <li key={index} className="mh8 mv8">
+            <div>
+              <Link href={result.link}>
+                <Card>
+                  <div className="result-card">
+                    <h2>{result.title}</h2>
+                    <p>{result.snippet}</p>
+                  </div>
+                </Card>
+              </Link>
+            </div>
+          </li>
+        )) : null}
+      </ul>
+    </Fragment>
+  )
+}
+
+
+export default Search
--- react/clients/cse.ts
+++ react/clients/cse.ts
@@ -1,12 +1,10 @@
 import axios from 'axios'
 import { CSE_CX, CSE_KEY } from '../utils/constants'
 
-const cseSearch = async (query: string) => {
+export const cseSearch = async (query: string) => {
   const res = await axios(`
      https://www.googleapis.com/customsearch/v1?q=${query}&cx=${CSE_CX}&key=${CSE_KEY}
   `)
   const results = await res.data.items
   return results
 }
-
-export default cseSearch
